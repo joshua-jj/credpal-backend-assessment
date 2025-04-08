c177bc50a4be52950eed2c7bf73522fb0901fe17
Joshua Akhidenor <akhidenorjoshua@gmail.com>
2025-04-08 01:00:38 +0100

    Configure pipes

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app
-     .useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
-     .useGlobalFilters(new HttpExceptionFilter())
-     .setGlobalPrefix('api')
-     .enableCors();
+  app
+    .useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    )
+    .useGlobalFilters(new HttpExceptionFilter())
+    .setGlobalPrefix('api')
+    .enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
