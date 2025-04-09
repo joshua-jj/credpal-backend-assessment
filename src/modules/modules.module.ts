ddc112ac3547057798fa2e436d5a999af0a3dca5
Joshua Akhidenor <akhidenorjoshua@gmail.com>
2025-04-09 17:47:29 +0100

    Add transactions

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [AuthenticationModule, UsersModule]
+  imports: [AuthenticationModule, UsersModule, TransactionsModule]
 })
 export class ModulesModule {}
