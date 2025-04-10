fda8ed0b5a02c12a038567681eb01923ee929a2c
Joshua Akhidenor <akhidenorjoshua@gmail.com>
2025-04-10 14:09:36 +0100

    Add util

--- src/common/utils/helper.util.ts
+++ src/common/utils/helper.util.ts
@@ -1,6 +1,6 @@
-
 import { User } from '@modules/users/entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 
 export class HelperUtil {
   public static parseApiResponse = (
@@ -16,9 +16,12 @@ export class HelperUtil {
     return await bcrypt.hash(password, salt);
   };
 
-  public static isPasswordsMatched = async (password: string, referencedPassword: string) =>  {
-    return await bcrypt.compare(password, referencedPassword)
-  }
+  public static isPasswordsMatched = async (
+    password: string,
+    referencedPassword: string,
+  ) => {
+    return await bcrypt.compare(password, referencedPassword);
+  };
 
   public static createJwtPayload = (user: User) => {
     return {
@@ -27,7 +30,9 @@ export class HelperUtil {
     };
   };
 
-  public static generateWalletId = (): `WAL${string}` => {
-    return `WAL`;
-  }
+  public static generateWalletId = (): `W${string}` => {
+    const uuid = uuidv4().replaceAll('-', '');
+    const shortId = uuid.slice(0, 8).toUpperCase();
+    return `W-${shortId}`;
+  };
 }
