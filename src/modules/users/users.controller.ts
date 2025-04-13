b02f46d5518aee9ce3b02ac8c6bcd5a8cf816960
Joshua Akhidenor <akhidenorjoshua@gmail.com>
2025-04-13 06:02:10 +0100

    Add endpoint to get profile

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,21 @@
-import {
-  Controller
-} from '@nestjs/common';
+import { Controller, Get, HttpStatus, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
+import { CurrentUser } from '@common/decorators/current-user.decorator';
+import { HelperUtil } from '@common/utils/helper.util';
 
-@Controller('')
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Get user profile' })
+  @Get('profile')
+  async getProfile(@CurrentUser('id', ParseIntPipe) userId: number) {
+    const data = this.usersService.getProfile(userId);
+    return HelperUtil.parseApiResponse(
+      HttpStatus.OK,
+      'Profile gotten successfully',
+      data,
+    );
+  }
 }
